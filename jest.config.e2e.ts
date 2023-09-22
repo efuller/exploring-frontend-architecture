60ad505f3f63ce9bc95e911694d09e3c6e3e12f6
Eric Fuller <info@glassmoonmedia.com>
2023-09-22 06:49:37 -0400

    Install puppeteer

--- jest.config.e2e.ts
+++ jest.config.e2e.ts
@@ -1,6 +1,8 @@
 import { Config } from 'jest'
 
 const config: Config = {
+  maxWorkers: 1,
+  preset: "jest-puppeteer",
   testMatch: ['**/@(src|tests)/**/*.@(e2e).*'],
   transform: {
     '^.+\\.(t|j)sx?$': ['ts-jest', {}],

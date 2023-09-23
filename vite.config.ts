87512a0bcfa8428b874ecb69894fd480d46debc8
Eric Fuller <info@glassmoonmedia.com>
2023-09-23 17:36:59 -0400

    Set a host

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   preview: {
+    host: '0.0.0.0',
     port: 10000,
   },
   plugins: [react()],

d87ab3181549a8a3614dfdd1a0c264750287af22
Eric Fuller <info@glassmoonmedia.com>
2023-09-23 17:54:29 -0400

    Try using cli

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  preview: {
-    host: '0.0.0.0',
-    port: 10000,
-  },
   plugins: [react()],
 })

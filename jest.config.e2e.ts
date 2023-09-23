db0993d31cb7035eb869cb57575c680ed124237f
Eric Fuller <info@glassmoonmedia.com>
2023-09-23 14:39:59 -0400

    Bump the timout for Jest

--- jest.config.e2e.ts
+++ jest.config.e2e.ts
@@ -4,6 +4,7 @@ const config: Config = {
   maxWorkers: 1,
   preset: "jest-puppeteer",
   testMatch: ['**/@(src|tests)/**/*.@(e2e).*'],
+  testTimeout: 30000,
   transform: {
     '^.+\\.(t|j)sx?$': ['ts-jest', {}],
   },

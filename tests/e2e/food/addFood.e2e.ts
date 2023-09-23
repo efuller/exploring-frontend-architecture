74be3bf5441b3ef0490843f60e2ecded3998367a
Eric Fuller <info@glassmoonmedia.com>
2023-09-23 14:41:19 -0400

    Get our first e2e test passing!

--- tests/e2e/food/addFood.e2e.ts
+++ tests/e2e/food/addFood.e2e.ts
@@ -1,18 +1,18 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
-import { FoodModel } from "../../../src/models/foodModel";
 import { MainPage } from "../../shared/pages/mainPage";
 import { PuppeteerPageDriver } from "../../shared/driver/pupeteerPageDriver";
+import { createFoodDto } from "../../../src/dto/createFoodDto";
 
 const feature = loadFeature('tests/e2e/food/addFood.feature');
 
 defineFeature(feature, (test) => {
   test('Adding a food', ({ given, when, then }) => {
-    let newFoodInput: FoodModel;
+    let newFoodInput: createFoodDto;
     let pageDriver: PuppeteerPageDriver;
     let mainPage: MainPage;
 
     beforeAll(async () => {
-      pageDriver = await PuppeteerPageDriver.create({ headless: false });
+      pageDriver = await PuppeteerPageDriver.create({ headless: false, slowMo: 100 });
       mainPage = new MainPage(pageDriver);
     });
 
@@ -23,15 +23,18 @@ defineFeature(feature, (test) => {
     given('The app can be accessed', async () => {
       // Do a thing
       await mainPage.open();
-      await pageDriver.pause();
     });
 
     when('The user adds a new food called steak', async () => {
+      newFoodInput = {
+        title: 'steak'
+      }
+
       await mainPage.addNewFood(newFoodInput);
     });
 
-    then('The user should be able to verify that steak is added to the list', () => {
-      expect(mainPage.isOnPage(newFoodInput)).toBe(true);
+    then('The user should be able to verify that steak is added to the list', async () => {
+      expect(await mainPage.foodTitleToBeInList(newFoodInput.title)).toBe(true);
     });
   });
 });

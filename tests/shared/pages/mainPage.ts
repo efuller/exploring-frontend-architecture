70d3f3c3d97239c4c614dfe3bc7c9e7c63ca41fb
Eric Fuller <info@glassmoonmedia.com>
2023-09-23 14:41:01 -0400

    Add in a PageModel

---
+++ tests/shared/pages/mainPage.ts
@@ -0,0 +1,53 @@
+import { PuppeteerPageDriver } from "../driver/pupeteerPageDriver";
+import { PageComponents } from "../pageComponents";
+import { createFoodDto } from "../../../src/dto/createFoodDto";
+
+export class MainPage {
+  private components: PageComponents;
+
+  public constructor(private pageDriver: PuppeteerPageDriver) {
+    this.components = new PageComponents({
+      driver: pageDriver,
+      timeout: 3000,
+      elements: {
+        foodInput: {
+          selector: '#food-input',
+          type: 'input'
+        },
+        submitBtn: {
+          selector: '#submit-btn',
+          type: 'button'
+        }
+      }
+    });
+  }
+
+  async addNewFood(newFood: createFoodDto) {
+    await this.components.load();
+    await this.components.get('foodInput').type(newFood.title);
+    await this.components.get('submitBtn').click();
+  }
+
+  async foodTitleToBeInList(food: string) {
+    const ul = await this.pageDriver.getPage().waitForSelector('#food-list', { timeout: 3000 });
+
+    if (!ul) {
+      return false;
+    }
+
+    const li = await ul.$$eval('li > p', (options) => {
+      return options.map((option) => option.textContent);
+    });
+
+    if (li.includes(food)) {
+      return true;
+    }
+    return false;
+  }
+
+  async open() {
+    const page = this.pageDriver.getPage();
+    // TODO: Make this configurable.
+    await page.goto('http://localhost:5173');
+  }
+}

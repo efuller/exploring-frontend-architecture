b1b115a8f1bb7ca91e58bd17fcb337def82dee71
Eric Fuller <info@glassmoonmedia.com>
2023-09-23 21:01:37 -0400

    Try setting a local URL as well

--- tests/shared/pages/mainPage.ts
+++ tests/shared/pages/mainPage.ts
@@ -46,11 +46,11 @@ export class MainPage {
   }
 
   async open() {
-    // let url = 'http://localhost:5173';
+    let url = 'https://explore-frontend-architecture.onrender.com/';
 
-    // if (process.env.NODE_ENV === 'production') {
-    const url = 'https://explore-frontend-architecture.onrender.com/';
-    // }
+    if (process.env.NODE_ENV === 'development') {
+      url = 'http://localhost:5173';
+    }
     const page = this.pageDriver.getPage();
     // TODO: Make this configurable.
     await page.goto(url);

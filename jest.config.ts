d290aa91331f8f149884e6909e4ab59c0096c7cb
Eric Fuller <info@glassmoonmedia.com>
2024-02-04 10:18:04 -0500

    Refactor jest config and scripts

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import { Config } from 'jest'
+
+const config: Config = {
+  testTimeout: 30000,
+  maxWorkers: 1,
+  projects: [
+    {
+      displayName: 'e2e',
+      preset: "jest-puppeteer",
+      testMatch: ['**/@(src|tests)/**/*.@(e2e).*'],
+      transform: {
+        '^.+\\.(t|j)sx?$': ['ts-jest', {}],
+      },
+    },
+  ],
+}
+
+export default config;

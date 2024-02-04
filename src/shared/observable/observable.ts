9ad651b9d4866a0a5208d2e1807d7438d22b615b
Eric Fuller <info@glassmoonmedia.com>
2024-02-04 11:23:54 -0500

    Add in an Observable

---
+++ src/shared/observable/observable.ts
@@ -0,0 +1,30 @@
+type Observer<T> = (observable: Observable<T>) => void;
+
+export default class Observable<T> {
+  value : T;
+  observers : Observer<T>[] = []
+
+  constructor(initialValue: T) {
+    this.value = initialValue;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  setValue(newValue : T) {
+    this.value = newValue;
+    this.notify();
+  }
+
+  subscribe(observer: Observer<T>) {
+    this.observers.push(observer);
+    this.notify();
+  }
+
+  notify() {
+    this.observers.forEach(observer => {
+      observer(this)
+    });
+  }
+}

7058ff0cd6b9b0e73232e515ec6c0378aeac18dc
Eric Fuller <info@glassmoonmedia.com>
2024-02-04 11:24:55 -0500

    Check if on the homepage

--- tests/app/journal/journal.app.ts
+++ tests/app/journal/journal.app.ts
@@ -1,11 +1,21 @@
 import { defineFeature, loadFeature } from "jest-cucumber";
+import { CompositionRoot } from "../../../src/shared/compositionRoot/compositionRoot";
+import { App } from "../../../src/shared/app/app";
 
 const feature = loadFeature('tests/app/journal/journal.app.feature');
 
 defineFeature(feature, (test) => {
+  let compositionRoot: CompositionRoot;
+  let app: App;
+
+  beforeEach(() => {
+    compositionRoot = new CompositionRoot();
+    app = compositionRoot.getApp();
+  });
+
   test('I can create a new journal entry', ({given, when, then}) => {
     given('I am on the homepage page', () => {
-      console.log('hi there');
+      expect(app.getCurrentRouteId()).toBe('home');
     });
 
     when(/^I add a new journal called "(.*)"$/, (entry) => {

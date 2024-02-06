ce9d06d00fd0e83848f66d41f693a66594e128a7
Eric Fuller <info@glassmoonmedia.com>
2024-02-06 07:51:32 -0500

    foodId should be journal

--- src/modules/journal/clientStorageRepository.ts
+++ src/modules/journal/clientStorageRepository.ts
@@ -1,7 +1,7 @@
 import { Journal } from "./journal.ts";
 
 export interface ClientStorageRepository {
-  add: (foodId: Journal) => Promise<void>;
+  add: (journal: Journal) => Promise<void>;
   delete: (id: string) => Promise<void>;
   getAll: () => Promise<Journal[]>;
 }

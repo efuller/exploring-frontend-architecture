327ab808c0dc09b400bff4b62e72cd38d62b0c6f
Eric Fuller <info@glassmoonmedia.com>
2024-02-08 06:30:00 -0500

    Send in proper props and adjust functionality of modal

--- src/components/ConfirmationModal/index.tsx
+++ src/components/ConfirmationModal/index.tsx
@@ -1,21 +1,20 @@
 import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { CheckIcon } from '@heroicons/react/24/outline'
-import { ConfirmationModal as ConfirmationModalLogic } from "../../shared/confirmationModal/confirmationModal.ts";
 
 type ConfirmationModalProps = {
   cancelButtonRef: React.RefObject<HTMLButtonElement>;
   onConfirm: () => void;
-  confirmationModal: ConfirmationModalLogic;
+  onCancel: () => void;
+  showModal: boolean;
+  setShowModal: (open: boolean) => void;
 }
 
-export const ConfirmationModal = ({ confirmationModal, onConfirm, cancelButtonRef }: ConfirmationModalProps) => {
+export const ConfirmationModal = ({ showModal, setShowModal, onConfirm, onCancel, cancelButtonRef }: ConfirmationModalProps) => {
 
   return (
-    <Transition.Root show={confirmationModal.isOpen()} as={Fragment}>
-      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={() => {
-        confirmationModal.closeModal();
-      }}>
+    <Transition.Root show={showModal} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setShowModal}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -61,7 +60,7 @@ export const ConfirmationModal = ({ confirmationModal, onConfirm, cancelButtonRe
                   <button
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-                    onClick={() => confirmationModal.closeModal()}
+                    onClick={onCancel}
                     ref={cancelButtonRef}
                   >
                     Cancel

3a08f848be288a14c0c3ab11e19177bc37511095
Eric Fuller <info@glassmoonmedia.com>
2024-02-08 06:17:09 -0500

    Remove code we are no longer using at the moment

--- src/hooks/useJournals/index.ts
+++ src/hooks/useJournals/index.ts
@@ -1,51 +1,11 @@
 import { useState } from "react";
-import axios, { AxiosResponse } from "axios";
-
-import { Journal } from "../../modules/journal/journal.ts";
+import { JournalState } from "../../modules/journal/journalRepository.ts";
 
 export const useJournals = () => {
-  const [journals, setJournals] = useState<Journal[]>([]);
-  const [pendingDelete, setPendingDelete] = useState<Journal | null>(null);
-
-  const createJournal = async (newJournal: Journal) => {
-    const result: AxiosResponse<Journal> = await axios.post('/journals', newJournal)
-
-    if (result.status === 201) {
-      setJournals((prevState) => {
-        return [...prevState, result.data];
-      });
-    }
-
-    return result;
-  }
-
-  const deleteJournal = async (id: string) => {
-    const result: AxiosResponse<Journal> = await axios.delete(`/journals/${id}`)
-
-    if (result.status === 200) {
-      const removeDeletedJournal = journals.filter((journal) => journal.id !== result.data.id);
-
-      setJournals(() => {
-        return [...removeDeletedJournal];
-      });
-    }
-
-    return result;
-  }
-
-  const exists = (id: string) => {
-    const items = journals.filter((journal) => journal.id === id);
-
-    return !!items.length;
-  }
+  const [journals, setJournals] = useState<JournalState>({journals: [], pendingDeletion: null});
 
   return {
-    createJournal: createJournal,
-    deleteJournal: deleteJournal,
-    exists,
-    pendingDelete,
     journals,
-    setPendingDelete,
     setJournals
   }
 }

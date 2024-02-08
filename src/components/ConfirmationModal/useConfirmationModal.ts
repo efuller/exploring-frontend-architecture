6f42ed742c7058d8bebbb6b1b2306f170235df84
Eric Fuller <info@glassmoonmedia.com>
2024-02-08 06:29:24 -0500

    Rename show to showModal

--- src/components/ConfirmationModal/useConfirmationModal.ts
+++ src/components/ConfirmationModal/useConfirmationModal.ts
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 
 export const useConfirmationModal = (isOpen = false) => {
-  const [open, setOpen] = useState(isOpen)
+  const [showModal, setShowModal] = useState(isOpen)
   const [confirmed, setConfirmed] = useState(false);
 
   const cancelButtonRef = useRef(null)
@@ -9,8 +9,8 @@ export const useConfirmationModal = (isOpen = false) => {
   return {
     cancelButtonRef,
     confirmed,
-    open,
+    showModal,
     setConfirmed,
-    setOpen,
+    setShowModal,
   }
 }

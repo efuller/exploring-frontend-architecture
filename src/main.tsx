2622656495868197c41654e0b78305f907456b48
Eric Fuller <info@glassmoonmedia.com>
2024-02-12 20:25:00 -0500

    Remove MSW

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-// import { setupBrowserMocks } from "./mocks/handlers.ts";
 import './index.css'
 import { CompositionRoot } from "./shared/compositionRoot/compositionRoot.ts";
-
-// setupBrowserMocks();
 
 const appRoot = new CompositionRoot();
 const journalModule = appRoot.getApp().getJournalModule();
